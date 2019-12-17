274942ef69848bc94ecce190931799e2eb97d133
sufyaniqbal <sufyaniqbal@me.com>
2019-12-17 14:58:45 +0000

    working progress

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,35 @@
 package main //1
 
 import "fmt" //2
+import "os"
+
+type player struct{
+	health int
+	name string
+	currentFloor int
+}
+
 
 func main() { //3
-	fmt.Println("Hello World") //4
+	var startingFloor int = 4
+	var gameOver = false
+	var currentPlayer player  
+	currentPlayer.currentFloor = startingFloor 
+	
+	for gameOver==false{
+		
+		fmt.Print("Pick a Floor : ")
+		var input int
+		e, err := fmt.Scanln(&input)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		currentPlayer.currentFloor = e
+		if currentPlayer.currentFloor == 0 {
+			gameOver=true
+			fmt.Println("Damn you Win again")
+		}
+	} 
+
 }
